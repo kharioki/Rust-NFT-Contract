86c049dc4deed62c2b3fb7980f45970a58f58aa1
tony <antolisah@gmail.com>
2022-01-28 15:57:03 +0300

    update minting logic

--- nft-contract/src/mint.rs
+++ nft-contract/src/mint.rs
@@ -5,36 +5,35 @@ impl Contract {
     #[payable]
     pub fn nft_mint(
         &mut self,
-        token_id: Option<TokenId>,
+        token_id: TokenId,
         metadata: TokenMetadata,
-        receiver_id: Option<AccountId>,
+        receiver_id: AccountId,
     ) {
-        // measure the initial storage being used on the contract
+        //measure the initial storage being used on the contract
         let initial_storage_usage = env::storage_usage();
 
-        // specify the token struct that contains the owner ID
+        //specify the token struct that contains the owner ID 
         let token = Token {
-            // set the owner ID equal to the receiver ID passed into the function
+            //set the owner ID equal to the receiver ID passed into the function
             owner_id: receiver_id,
         };
 
-        // insert the token ID and token struct and make sure the token doesn't exist
+        //insert the token ID and token struct and make sure that the token doesn't exist
         assert!(
             self.tokens_by_id.insert(&token_id, &token).is_none(),
             "Token already exists"
         );
 
-        // insert the token ID and metadata
+        //insert the token ID and metadata
         self.token_metadata_by_id.insert(&token_id, &metadata);
 
-        // calls the internal method for adding the token to the owner
+        //call the internal method for adding the token to the owner
         self.internal_add_token_to_owner(&token.owner_id, &token_id);
 
-        // calculate the required storage which was used - the initial storage
+        //calculate the required storage which was the used - initial
         let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
 
-        // refund any excess storage if the user attached too much.
-        // Panic if they didn't attach enough to cover the required
+        //refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
         refund_deposit(required_storage_in_bytes);
     }
 }
